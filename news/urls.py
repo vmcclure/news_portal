845961e5e05014e0884a5d4945457fce9a9e2d66
Dominic D <nikitin2@list.ru>
2020-06-10 18:42:43 +0300

    html magic

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,get_category
 
 urlpatterns = [
-    path('', index)
+    path('', index, name="Home"),
+    path('category/<int:category_id>/', get_category, name= 'category')
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 # Create your views here.
 
 
 def index(request):
     news = News.objects.order_by('-create_at')
-    return render(request, 'news/index.html', {'news' : news, 'title' : 'Список новостей'})
+
+    return render(request, 'news/index.html', {'news' : news,
+                                               'title' : 'Список новостей',
+                                               })
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news' : news,
+
+                                               'category' : category,})
